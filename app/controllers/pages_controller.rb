995e7554118d05aeeb96b01599934c8473ce9da5
lowpez <lopezaguayo@aol.com>
2021-08-10 14:56:10 -0400

    Ajustes menores en routes y pages_controller para mostrar Paginas estaticas

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: [:home, :services]
 
   def home
+  end
+
+  def services
   end
 end
