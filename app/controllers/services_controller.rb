640fa21a69ade5033c186bc411f1592fe534b599
lowpez <lopezaguayo@aol.com>
2021-08-11 17:55:03 -0400

    Fixes y creaciones de vistas

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -2,15 +2,24 @@ class ServicesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
+    @service = Service.all
   end
 
   def show
+    @service = Service.find(params[:id])
   end
 
   def new
+    @service = Service.new
   end
 
   def create
+    @service = Service.new(service_params)
+    if @service.save
+      redirect_to service_path(@service)
+    else
+      render "new"
+    end
   end
 
   def edit
@@ -20,5 +29,11 @@ class ServicesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def service_params
+    params.require(:service).permit(:title, :descripcion, :tarifa, :tiempo_ejecucion)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :services
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/services',    to: 'pages#services'
+  # get '/services',    to: 'pages#services'
 end
