5c51d7b07567fdd23f6c2e74c8b1466306da61ef
tahia campos <tahia.campos@gmail.com>
2021-08-17 16:46:51 -0400

    creando index pages

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   get '/search', to: 'flats#search'
   devise_for :users
   root to: 'pages#home'
-  resources :home, only: :home
+  resources :pages
+  resources :home, only: [:home, :index] 
   resources :categories, only: [:home, :index, :show]
   resources :services
   resources :flats
