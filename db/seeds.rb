d3ef248a65a36093ba103ac6a4697f463b1cbc95
tahia campos <tahia.campos@gmail.com>
2021-08-17 11:14:25 -0400

    agregando seeds y cards de servicios al home

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Service.create(user_id: 1, category_id: 1, title: "Limpieza de oficinas", description: "Limpieza profunda para sus oficinas", price: 20000, execution_time: 2, warranty: "si", photo: "primera foto")
+Service.create(user_id: 1, category_id: 1, title: "Limpieza de autos", description: "Limpieza profunda para sus autos", price: 30000, execution_time: 2, warranty: "si", photo: "segunda foto")
+Service.create(user_id: 1, category_id: 1, title: "Limpieza de aviones", description: "Limpieza profunda para sus aviones", price: 500000, execution_time: 2, warranty: "si", photo: "tercera foto")
+Service.create(user_id: 1, category_id: 1, title: "Limpieza de transbordador", description: "Limpieza profunda para sus transbordador", price: 1000000, execution_time: 2, warranty: "si", photo: "cuarta foto")
