fb0e9452c73646e21e0e3d67cfc517b0887bfadc
AndresIbanez <andresibanezulloa@gmail.com>
2021-09-03 16:00:03 -0400

    booking 1

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,20 +1,28 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: [:new, :create]
 
-
+  def new
+    @book = Booking.new
+  end
 
   def create
     @book = Booking.new(booking_params)
-    # we need `restaurant_id` to associate review with corresponding restaurant
-    @service = Service.find(params[:id])
     @book.service = @service
-    @book.save
-    redirect_to root_path(@service)
+    @book.user = current_user
+    if @book.save
+      redirect_to root_path
+    end
+
+    
   end
 
   private
 
-  def booking_params
-    params.require(:review).permit(:current_user.id, :@service.id)
+  def set_booking
+    @service = Service.find(params[:service_id])
   end
 
+  def booking_params
+    params.require(:booking).permit(:payment_method, :service_id, :current_user)
+  end
 end
